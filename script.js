ebdff47a8c89fe2b8c05129e71e0d0b57cff6905
Aminulislam98 <aminurrohman98@gmail.com>
2025-09-20 22:02:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const api_url = "http://api.quotable.io/random";
+
+async function getQuote(url) {
+  const quote = document.getElementById("quote");
+  const author = document.getElementById("author");
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
+
+getQuote(api_url);
